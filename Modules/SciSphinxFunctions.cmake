6172ef737a7b3de17e5f4b1baf41d184baeae1c8
krugers <krugers@03405629-b146-45b9-a56f-558d5653c1c1>
2012-10-27 15:46:28 +0000

    This adds the install target functionality to the sphinx documentation.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/scimake/trunk@84 03405629-b146-45b9-a56f-558d5653c1c1

--- Modules/SciSphinxFunctions.cmake
+++ Modules/SciSphinxFunctions.cmake
@@ -19,17 +19,26 @@ include(CMakeParseArguments)
 # Define the target for making HTML
 # Args:
 #   TARGET:  Name to make the target.  Actual target will be #   ${TARGET_NAME}-html
-#   LATEX_ROOT:  Root name of Latex file.  From conf.py
+#   RST_FILE_BASE:  Root name of Latex file.  From conf.py
 #   SOURCE_DIR:  Directory containing the index.rst.  Defaults to CMAKE_CURRENT_SOURCE_DIR
 #   SPHINX_ADDL_OPTS:  Additional options to Sphinx
 #   FILE_DEPS:  Files that are the dependencies
+#   SPHINX_BUILDS: Which builds to include.  Default is "html latex pdf "
+#    Possible choices are "html latex pdf singlehtml man"
+#   SPHINX_INSTALLS: Which builds to install.  Default is same as builds
+#   NOWARN_NOTMATCH_DIR: Do not warn if file base does not match install dir
+#   INSTALL_SUPERDIR: Name of installation directory up to this one.
+#                     Should not be absolute (not include prefix).
+#                     Overridden by INSTALL_SUBDIR.
+#   INSTALL_SUBDIR:   Name of this subdir for installation.
+#                   Should not be absolute (not include prefix).
 #
 macro(SciSphinxTarget)
 
 # Parse out the args
-  set(opts DEBUG) # no-value args
-  set(oneValArgs LATEX_ROOT;TARGET;SPHINX_ADDL_OPTS;SOURCE_DIR)
-  set(multValArgs FILE_DEPS) # e.g., lists
+  set(opts DEBUG;NOWARN_NOTMATCH_DIR) # no-value args
+  set(oneValArgs RST_FILE_BASE;TARGET;SPHINX_ADDL_OPTS;SOURCE_DIR;INSTALL_SUPERDIR;INSTALL_SUBDIR)
+  set(multValArgs FILE_DEPS;ALL_BUILDS) # e.g., lists
   cmake_parse_arguments(FD "${opts}" "${oneValArgs}" "${multValArgs}" ${ARGN})
 
   ###
@@ -38,30 +47,54 @@ macro(SciSphinxTarget)
   if(NOT DEFINED FD_SOURCE_DIR)
     set(FD_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR})
   endif()
+  if(NOT DEFINED FD_SPHINX_BUILDS)
+    set(FD_SPHINX_BUILDS)
+    list(APPEND FD_SPHINX_BUILDS html latex pdf)
+  endif()
+  if(NOT DEFINED FD_SPHINX_INSTALLS)
+    set(FD_SPHINX_INSTALLS ${FD_SPHINX_BUILDS})
+  endif()
+  if (FD_INSTALL_SUBDIR)
+    set(instdir ${DOC_INSTALL_SUBDIR})
+  elseif (DOC_INSTALL_SUPERDIR)
+    set(instdir ${DOC_INSTALL_SUPERDIR}/${thissubdir})
+  else ()
+    set(instdir share)
+  endif ()
   ###
-  ##  Basic idiot checks
+  ##  Basic sanity checks
   #
+  get_filename_component(thissubdir ${CMAKE_CURRENT_SOURCE_DIR} NAME)
+  if (NOT NOWARN_NOTMATCH_DIR)
+    set(WARN_NOTMATCH_DIR)
+  endif ()
+  if (WARN_NOTMATCH_DIR)
+    if (NOT "${thissubdir}" STREQUAL "${FD_RST_FILE_BASE}")
+      message(WARNING "Main rst file base, ${FD_RST_FILE_BASE}, does not match subdirectory name, ${thissubdir}.")
+    endif ()
+  endif ()
+
   if(NOT DEFINED FD_TARGET)
-     message(WARNING "SciSphinxHtmlTarget called without specifying the target name")
+     message(WARNING "SciSphinxTarget called without specifying the target name")
      return()
   endif()
   if(NOT DEFINED FD_FILE_DEPS)
-     message(WARNING "SciSphinxHtmlTarget called without specifying the file dependencies")
+     message(WARNING "SciSphinxTarget called without specifying the file dependencies")
      return()
   endif()
-  if(NOT DEFINED FD_LATEX_ROOT)
-     message(WARNING "SciSphinxHtmlTarget called without specifying the latex root from conf.py")
+  if(NOT DEFINED FD_RST_FILE_BASE)
+     message(WARNING "SciSphinxTarget called without specifying the latex root from conf.py")
      return()
   endif()
   if(NOT DEFINED Sphinx_EXECUTABLE)
-     message(WARNING "SciSphinxHtmlTarget called without defining Sphinx_EXECUTABLE")
+     message(WARNING "SciSphinxTarget called without defining Sphinx_EXECUTABLE")
      return()
   endif()
   if (FD_DEBUG)
     message("")
-    message("--------- SciSphinxHtmlTarget defining ${FD_TARGET}-html ---------")
+    message("--------- SciSphinxTarget defining ${FD_TARGET}-html ---------")
     message(STATUS "[SciSphinxFunctions]: TARGET= ${FD_TARGET} ")
-    message(STATUS "[SciSphinxFunctions]: LATEX_ROOT= ${FD_LATEX_ROOT} ")
+    message(STATUS "[SciSphinxFunctions]: RST_FILE_BASE= ${FD_RST_FILE_BASE} ")
     message(STATUS "[SciSphinxFunctions]: Sphinx_EXECUTABLE= ${Sphinx_EXECUTABLE} ")
     message(STATUS "[SciSphinxFunctions]: Sphinx_OPTS= ${Sphinx_OPTS} ")
     message(STATUS "[SciSphinxFunctions]: SPHINX_ADDL_OPTS= ${FD_SPHINX_ADDL_OPTS} ")
@@ -71,11 +104,11 @@ macro(SciSphinxTarget)
   #
   set(html_OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/html/index.html)
   set(singlehtml_OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/singlehtml/index.html)
-  set(latex_OUTPUT ${BLDDIR}/pdf/${FD_LATEX_ROOT}.tex)
-  set(pdf_OUTPUT ${BLDDIR}/pdf/${FD_LATEX_ROOT}.pdf)
+  set(latex_OUTPUT ${BLDDIR}/pdf/${FD_RST_FILE_BASE}.tex)
+  set(pdf_OUTPUT ${BLDDIR}/pdf/${FD_RST_FILE_BASE}.pdf)
   set(man_OUTPUT ${BLDDIR}/man/index.man)
 
-  foreach (build html latex singlehtml man)
+  foreach (build ${FD_SPHINX_BUILDS})
     set (${build}_DIR ${CMAKE_CURRENT_BINARY_DIR}/${build})
     # Latex is actually for pdf which is below
     if(${build} STREQUAL latex)
@@ -86,12 +119,14 @@ macro(SciSphinxTarget)
     # so this method fixes the problems that arise if Sphinx_OPTS is not defined
     set(all_opts -b ${build} ${Sphinx_OPTS} ${FD_SPHINX_ADDL_OPTS})
 
-    add_custom_command(
-      OUTPUT ${${build}_OUTPUT}
-      COMMAND ${Sphinx_EXECUTABLE} ${all_opts} ${FD_SOURCE_DIR} ${${build}_DIR}
-      DEPENDS ${FD_FILE_DEPS}
-    )
-    add_custom_target(${FD_TARGET}-${build} DEPENDS ${${build}_OUTPUT})
+    if(NOT ${build} STREQUAL pdf)
+      add_custom_command(
+        OUTPUT ${${build}_OUTPUT}
+        COMMAND ${Sphinx_EXECUTABLE} ${all_opts} ${FD_SOURCE_DIR} ${${build}_DIR}
+        DEPENDS ${FD_FILE_DEPS}
+      )
+      add_custom_target(${FD_TARGET}-${build} DEPENDS ${${build}_OUTPUT})
+    endif()
   endforeach()
 
   ###
@@ -106,24 +141,30 @@ macro(SciSphinxTarget)
   )
   add_custom_target(${FD_TARGET}-pdf DEPENDS ${pdf_OUTPUT})
 
+  ###
+  ##  Each install is a one-off
+  # 
+  list(FIND SPHINX_INSTALLS "pdf" indx)
+  if (NOT indx EQUAL -1)
+    install(FILES ${CMAKE_CURRENT_BINARY_DIR}/pdf/${FD_RST_FILE_BASE}.pdf
+      DESTINATION "${instdir}"
+      PERMISSIONS OWNER_WRITE OWNER_READ GROUP_WRITE GROUP_READ WORLD_READ
+    )
+  endif ()
+  list(FIND SPHINX_INSTALLS "html" indx)
+  if (NOT indx EQUAL -1)
+    install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/html
+      DESTINATION ${instdir}
+      FILE_PERMISSIONS OWNER_WRITE OWNER_READ GROUP_WRITE GROUP_READ WORLD_READ
+    )
+  endif ()
+  list(FIND SPHINX_INSTALLS "man" indx)
+  if (NOT indx EQUAL -1)
+    install(
+      DIRECTORY ${BLDDIR}/man
+      OPTIONAL
+      DESTINATION ${instdir}/man
+      COMPONENT userdocs
+    )
+  endif ()
 endmacro()
-
-# Need to figure out a better way of automating install as well, but worry about it later.
-if (0)
-# We install the html files directly under learning_vorpal
-install(
-  DIRECTORY ${BLDDIR}/html/
-  OPTIONAL
-  DESTINATION ${USERDOCS_INSTALLDIR}/learning_vorpal
-  COMPONENT userdocs
-)
-
-# This becomes a subdirectory because no trailing slash
-install(
-  DIRECTORY ${BLDDIR}/man
-  OPTIONAL
-  DESTINATION ${USERDOCS_INSTALLDIR}/learning_vorpal/
-  COMPONENT userdocs
-)
-endif ()
-
