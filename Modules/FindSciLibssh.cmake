9a8095d4123aa18520129e09017e2569e791d079
rooparanip <rooparanip@03405629-b146-45b9-a56f-558d5653c1c1>
2012-11-20 00:16:25 +0000

    Module to find libssh
    git-svn-id: https://ice.txcorp.com/svnrepos/code/scimake/trunk@94 03405629-b146-45b9-a56f-558d5653c1c1

---
+++ Modules/FindSciLibssh.cmake
@@ -0,0 +1,43 @@
+# - FindSciLibsshn: Module to find include directories and libraries
+#   for Libssh. This module was implemented as there is no stock
+#   CMake module for Libssh. This is currently being used by QuIDS
+#   project.
+#
+# This module can be included in CMake builds in find_package:
+#   find_package(SciLibssh REQUIRED)
+#
+# This module will define the following variables:
+#  HAVE_LIBSSH         = Whether have the Libssh library
+#  Libssh_INCLUDE_DIRS = Location of Libssh includes
+#  Libssh_LIBRARY_DIRS = Location of Libssh libraries
+#  Libssh_LIBRARIES    = Required libraries
+#  Libssh_STLIBS       = Location of Libssh static library
+
+######################################################################
+#
+# SciFindLibssh: find includes and libraries for Libssh.
+#
+# $Id: FindSciLibssh.cmake 58 2012-09-15 13:43:53Z jrobcary $
+#
+# Copyright 2010-2012 Tech-X Corporation.
+# Arbitrary redistribution allowed provided this copyright remains.
+#
+######################################################################
+set(SUPRA_SEARCH_PATH ${SUPRA_SEARCH_PATH})
+
+SciFindPackage(PACKAGE "Libssh"
+               INSTALL_DIRS "libssh"
+               HEADERS "libssh/libssh.h"
+               LIBRARIES "ssh"
+              )
+
+if (LIBSSH_FOUND)
+  message(STATUS "Found Libssh")
+  set(HAVE_LIBSSH 1 CACHE BOOL "Whether have the LIBSSH library")
+else ()
+  message(STATUS "Did not find Libssh.  Use -DLibssh_ROOT_DIR to specify the installation directory.")
+  if (SciLibssh_FIND_REQUIRED)
+    message(FATAL_ERROR "Failed.")
+  endif ()
+endif ()
+
