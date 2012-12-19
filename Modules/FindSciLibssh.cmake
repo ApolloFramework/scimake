e4457f53be4e4c7b6a13e3ac3159fb835857b01e
jrobcary <jrobcary@03405629-b146-45b9-a56f-558d5653c1c1>
2012-12-19 15:48:03 +0000

    FindSciLibssh.cmake can now be told to find the shared installation.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/scimake/trunk@112 03405629-b146-45b9-a56f-558d5653c1c1

--- Modules/FindSciLibssh.cmake
+++ Modules/FindSciLibssh.cmake
@@ -6,6 +6,10 @@
 # This module can be included in CMake builds in find_package:
 #   find_package(SciLibssh REQUIRED)
 #
+# Input variables
+# ENABLE_SHARED if true, will look for shared installation of libssh
+# USE_SHARED_LIBS same as above.
+#
 # This module will define the following variables:
 #  HAVE_LIBSSH         = Whether have the Libssh library
 #  Libssh_INCLUDE_DIRS = Location of Libssh includes
@@ -23,13 +27,21 @@
 # Arbitrary redistribution allowed provided this copyright remains.
 #
 ######################################################################
-set(SUPRA_SEARCH_PATH ${SUPRA_SEARCH_PATH})
 
-SciFindPackage(PACKAGE "Libssh"
-               INSTALL_DIRS "libssh"
-               HEADERS "libssh/libssh.h"
-               LIBRARIES "ssh"
-              )
+# set(SUPRA_SEARCH_PATH ${SUPRA_SEARCH_PATH})
+
+if (ENABLE_SHARED OR USE_SHARED_LIBS)
+  set(instdirs "libssh-cc4py;libssh-sersh")
+else ()
+  set(instdirs "libssh")
+endif ()
+
+SciFindPackage(
+  PACKAGE "Libssh"
+  INSTALL_DIRS ${instdirs}
+  HEADERS "libssh/libssh.h"
+  LIBRARIES "ssh"
+)
 
 if (LIBSSH_FOUND)
   message(STATUS "Found Libssh")
