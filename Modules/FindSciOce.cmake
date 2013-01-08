18fee98f1a11ecfcde01d7dd9801ea5d014afe80
jrobcary <jrobcary@03405629-b146-45b9-a56f-558d5653c1c1>
2013-01-08 15:14:40 +0000

    Adding module to find OCE.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/scimake/trunk@139 03405629-b146-45b9-a56f-558d5653c1c1

---
+++ Modules/FindSciOce.cmake
@@ -0,0 +1,35 @@
+# - FindOce: Module to find include directories and
+#   libraries for Opencascade Community Edition
+#
+# Module usage:
+#   find_package(Oce ...)
+#
+# This module will define the following variables:
+#  HAVE_TXBASE, TXBASE_FOUND = Whether libraries and includes are found
+#  Oce_INCLUDE_DIRS       = Location of Oce includes
+#  Oce_LIBRARY_DIRS       = Location of Oce libraries
+#  Oce_LIBRARIES          = Required libraries
+
+######################################################################
+#
+# FindOce: find includes and libraries for OCE
+#
+# $Id$
+#
+# Copyright 2010-2012 Tech-X Corporation.
+# Arbitrary redistribution allowed provided this copyright remains.
+#
+######################################################################
+
+set(Oce_SEARCHLIBS
+  TKBRep
+  TKPrim
+  TKSTEP TKSTEP209 TKSTEPAttr TKSTEPBase
+  TKSTL
+)
+
+SciFindPackage(PACKAGE "Oce"
+  HEADERS "TopoDS_Builder.hxx"
+  LIBRARIES "${Oce_SEARCHLIBS}"
+)
+
