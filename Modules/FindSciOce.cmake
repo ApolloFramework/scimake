d432e3223ec5803997406f40874daad371b5ecc6
jrobcary <jrobcary@03405629-b146-45b9-a56f-558d5653c1c1>
2013-01-10 21:41:01 +0000

    Fix a comment.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/scimake/trunk@147 03405629-b146-45b9-a56f-558d5653c1c1

--- Modules/FindSciOce.cmake
+++ Modules/FindSciOce.cmake
@@ -5,10 +5,10 @@
 #   find_package(Oce ...)
 #
 # This module will define the following variables:
-#  HAVE_TXBASE, TXBASE_FOUND = Whether libraries and includes are found
-#  Oce_INCLUDE_DIRS       = Location of Oce includes
-#  Oce_LIBRARY_DIRS       = Location of Oce libraries
-#  Oce_LIBRARIES          = Required libraries
+#  HAVE_OCE, OCE_FOUND = Whether libraries and includes are found
+#  Oce_INCLUDE_DIRS    = Location of Oce includes
+#  Oce_LIBRARY_DIRS    = Location of Oce libraries
+#  Oce_LIBRARIES       = Required libraries
 
 ######################################################################
 #
