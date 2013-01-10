b9646b41f60f6f01f9536cd000548baab44a8dd4
jrobcary <jrobcary@03405629-b146-45b9-a56f-558d5653c1c1>
2013-01-10 14:55:48 +0000

    OCE libraries layering in place to allow one to get, e.g., STEP on down.  Worked out only through IGES, STEP, and STL readers.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/scimake/trunk@145 03405629-b146-45b9-a56f-558d5653c1c1

--- Modules/FindSciOce.cmake
+++ Modules/FindSciOce.cmake
@@ -21,14 +21,57 @@
 #
 ######################################################################
 
-set(Oce_SEARCHLIBS
-  TKBRep
-  TKPrim
-  TKSTEP TKSTEP209 TKSTEPAttr TKSTEPBase
-  TKSTL
-  TKernel
-)
+# Uses of these libs found from CMakeLists.txt in OCE
+# Analyze dependencies using otool -L on OS X.
+# Below is a layered list, top to bottom, left to right.
+# ToDo: define the SEARCHHDRS
+set(OceIges_SEARCHLIBS TKIGES)
+# IGES dependency
+set(OceAdvGeom_SEARCHLIBS TKFillet TKBool TKPrim TKBO)
+set(OceStep_SEARCHLIBS TKSTEP TKSTEP209 TKSTEPAttr TKSTEPBase)
+# STEP and IGES depend on this, but not STL
+set(OceIoBase_SEARCHLIBS TKXSBase)
+set(OceStl_SEARCHLIBS TKSTL)
+set(OceAlgo_SEARCHLIBS TKShHealing TKTopAlgo TKGeomAlgo)
+set(OceGeom_SEARCHLIBS TKBrep TKGeomBase TKG3d TKG2d)
+set(OceTools_SEARCHLIBS TKMath TKAdvTools)
+set(OceKernel_SEARCHLIBS TKernel)
 
+# Enforce dependencies
+if (OceIges_FIND)
+  set(OceAdvGeom_FIND TRUE)
+  set(OceIoBase_FIND TRUE)
+endif ()
+if (OceStep_FIND)
+  set(OceIoBase_FIND TRUE)
+endif ()
+if (OceIoBase_FIND OR OceStl_FIND)
+  set(OceAlgo_FIND TRUE)
+endif ()
+if (OceAlgo_FIND)
+  set(OceGeom_FIND TRUE)
+endif ()
+if (OceGeom_FIND)
+  set(OceTools_FIND TRUE)
+endif ()
+if (OceTools_FIND)
+  set(OceKernel_FIND TRUE)
+endif ()
+
+# Set the libraries
+set(Oce_SEARCHLIBS)
+foreach (pkg Iges AdvGeom Step IoBase Stl Algo Geom Tools Kernel)
+  message(STATUS "Oce${pkg}_FIND = ${Oce${pkg}_FIND}.")
+  if (Oce${pkg}_FIND)
+    set(Oce_SEARCHLIBS ${Oce_SEARCHLIBS} ${Oce${pkg}_SEARCHLIBS})
+  endif ()
+endforeach ()
+message(STATUS "Oce_SEARCHLIBS = ${Oce_SEARCHLIBS}.")
+
+# Worry about data exchange later
+#  TKVRML TKXCAF TKXCAFSchema TKXmlXCAF TKBinXCAF TKXDEIGES TKXDESTEP
+
+# To Do: Set variables for each group individually
 SciFindPackage(PACKAGE "Oce"
   HEADERS "TopoDS_Builder.hxx"
   LIBRARIES "${Oce_SEARCHLIBS}"
