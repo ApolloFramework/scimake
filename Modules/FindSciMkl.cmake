fc541d2b7c937bef7656cbb420821005da1f9441
jrood <jrood@03405629-b146-45b9-a56f-558d5653c1c1>
2013-03-14 21:57:41 +0000

    Reversing last change.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/scimake/trunk@238 03405629-b146-45b9-a56f-558d5653c1c1

--- Modules/FindSciMkl.cmake
+++ Modules/FindSciMkl.cmake
@@ -45,7 +45,7 @@ else (WIN32)
   #foreach(year 2011 2012 2013)
     set(Mkl_ROOT_DIR "/usr/local/intel/mkl/lib/intel64")
       SciFindPackage(PACKAGE "Mkl"
-                    LIBRARIES "mkl_intel_lp64;mkl_intel_thread;mkl_core"
+                    LIBRARIES "mkl_rt"
                     )
     set(Iomp5_ROOT_DIR "/usr/local/intel/lib/intel64")
       SciFindPackage(PACKAGE "Iomp5"
