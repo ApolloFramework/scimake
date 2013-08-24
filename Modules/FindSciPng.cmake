249361728ee0bad507d791305cf924e3f0949396
jrobcary <jrobcary@03405629-b146-45b9-a56f-558d5653c1c1>
2013-08-24 11:46:26 +0000

    Adding ability to find the png library.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/scimake/trunk@344 03405629-b146-45b9-a56f-558d5653c1c1

---
+++ Modules/FindSciPng.cmake
@@ -0,0 +1,40 @@
+# - FindSciPng: Module to find include directories and
+#   libraries for Png.
+#
+# Module usage:
+#   find_package(SciPng ...)
+#
+# This module will define the following variables:
+#  HAVE_PNG, PNG_FOUND = Whether libraries and includes are found
+#  Png_INCLUDE_DIRS       = Location of Png includes
+#  Png_LIBRARY_DIRS       = Location of Png libraries
+#  Png_LIBRARIES          = Required libraries
+
+######################################################################
+#
+# SciFindPng: find includes and libraries for z(compression)
+#
+# $Id$
+#
+# Copyright 2010-2013 Tech-X Corporation.
+# Arbitrary redistribution allowed provided this copyright remains.
+#
+# See LICENSE file (EclipseLicense.txt) for conditions of use.
+#
+######################################################################
+
+SciFindPackage(
+  PACKAGE "Png"
+  INSTALL_DIRS zlib
+  LIBRARIES "png"
+)
+
+if (PNG_FOUND)
+  # message(STATUS "Found Png(compression library)")
+  set(HAVE_PNG 1 CACHE BOOL "Whether have the z(compression) library")
+else ()
+  message(STATUS "Did not find Png(compression).")
+  if (SciPng_FIND_REQUIRED)
+    message(FATAL_ERROR "Failed.")
+  endif ()
+endif ()
