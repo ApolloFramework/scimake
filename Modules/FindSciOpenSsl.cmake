0a429e0bd927bca44b4b9c6fdf23985ccfeece24
jrobcary <jrobcary@03405629-b146-45b9-a56f-558d5653c1c1>
2013-12-01 21:25:44 +0000

    Find stlibs and mdlibs.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/scimake/trunk@391 03405629-b146-45b9-a56f-558d5653c1c1

--- Modules/FindSciOpenSsl.cmake
+++ Modules/FindSciOpenSsl.cmake
@@ -28,28 +28,69 @@
 SciGetInstDirs(openssl instdirs)
 set(instdirs openssl openssl-sersh)
 
+# Libraries have different names on Windows.  Use static MD or MT libs.
+if (WIN32)
+  set(ssl_libs ssleay32 libeay32)
+else ()
+  set(ssl_libs ssl crypto)
+endif ()
+
 SciFindPackage(PACKAGE "OpenSsl"
   INSTALL_DIRS ${instdirs}
   EXECUTABLES openssl
   HEADERS openssl/ssl.h
-  LIBRARIES ssl crypto
-  INCLUDE_SUBDIRS include
+  LIBRARIES ${ssl_libs}
+  # INCLUDE_SUBDIRS include
+  # LIBRARY_SUBDIRS lib/VC/static lib
 )
 
 # On Windows, if failed, try again with system paths
 if (WIN32 AND NOT OPENSSL_FOUND)
   if (EXISTS C:/OpenSSL AND NOT OpenSsl_ROOT_DIR)
     set(OpenSsl_ROOT_DIR C:/OpenSSL)
+  else ()
+    find_program(openssl_exec openssl)
+    if (openssl_exec)
+      get_filename_component(openssl_bindir ${openssl_exec}/.. REALPATH)
+      get_filename_component(OpenSsl_ROOT_DIR ${openssl_bindir}/.. REALPATH)
+    endif ()
+  endif ()
+  if (OpenSsl_ROOT_DIR)
     SciFindPackage(PACKAGE "OpenSsl"
       INSTALL_DIRS ${instdirs}
       EXECUTABLES openssl
       HEADERS openssl/ssl.h
-      LIBRARIES ssl crypto
-      INCLUDE_SUBDIRS include
+      LIBRARIES ${ssl_libs}
+      # INCLUDE_SUBDIRS include
+      # LIBRARY_SUBDIRS lib/VC/static lib.  This search gives dll libs
     )
   endif ()
 endif ()
 
+# Correct static libraries on Windows
+if (WIN32)
+  set(srchlibs ${OpenSsl_LIBRARIES})
+  set(OpenSsl_STLIBS)
+  set(OpenSsl_MDLIBS)
+  foreach (lib ${srchlibs})
+    get_filename_component(openssl_libdir ${lib}/.. REALPATH)
+    get_filename_component(openssl_libname ${lib} NAME_WE)
+    # message(STATUS "Looking for ${openssl_libname}MD in ${openssl_libdir}/VC/static.")
+    find_library(mdlib ${openssl_libname}MD PATHS ${openssl_libdir}/VC/static
+      NO_DEFAULT_PATH
+    )
+    set(OpenSsl_MDLIBS ${OpenSsl_MDLIBS} ${mdlib})
+    find_library(stlib ${openssl_libname}MT PATHS ${openssl_libdir}/VC/static
+      NO_DEFAULT_PATH
+    )
+    set(OpenSsl_STLIBS ${OpenSsl_STLIBS} ${stlib})
+  endforeach ()
+  message(STATUS "After correction.")
+  SciPrintVar(OpenSsl_STLIBS)
+  SciPrintVar(OpenSsl_MDLIBS)
+endif ()
+
+# Finish up
 if (OPENSSL_FOUND)
   # message(STATUS "Found OpenSsl")
   set(HAVE_OPENSSL 1 CACHE BOOL "Whether have the OPENSSL library")
