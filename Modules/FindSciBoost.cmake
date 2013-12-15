f9e7ad6002357bbf352c562583e4bed4b935cd0e
jrobcary <jrobcary@03405629-b146-45b9-a56f-558d5653c1c1>
2013-12-15 06:31:01 +0000

    Fix finding dlls.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/scimake/trunk@411 03405629-b146-45b9-a56f-558d5653c1c1

--- Modules/FindSciBoost.cmake
+++ Modules/FindSciBoost.cmake
@@ -75,7 +75,7 @@ SciFindPackage(PACKAGE "Boost"
   LIBRARIES "${SciBoost_LIBRARY_LIST}"
 )
 unset(SciBoost_LIBRARY_LIST CACHE)
-if (Boost_FOUND_SOME_DLL)
+if (Boost_DLLS)
   message(STATUS "Correcting Boost DLL definition")
   set(Boost_DEFINITIONS -DBOOST_ALL_DYN_LINK)
   message(STATUS "Boost_DEFINITIONS = ${Boost_DEFINITIONS}.")
