afc31826da50913e8ad690ea60c307fd0794e2cd
techxdws <techxdws@03405629-b146-45b9-a56f-558d5653c1c1>
2014-01-03 01:15:21 +0000

    Added add_unit_test_with_comparison() macro.
    Should probably move to something like SciCTest.cmake or something like
    that. Will clean up.
    
    
    git-svn-id: https://ice.txcorp.com/svnrepos/code/scimake/trunk@440 03405629-b146-45b9-a56f-558d5653c1c1

--- SciInit.cmake
+++ SciInit.cmake
@@ -379,3 +379,24 @@ MACRO(add_unit_test)
   add_test(NAME ${TEST_NAME} COMMAND ${TEST_COMMAND} ${TEST_ARGS})
 ENDMACRO()
 
+MACRO(add_unit_test_with_comparison)
+  set(oneValArgs NAME COMMAND RESULTS_DIR STDOUT)
+  set(multiValArgs RESULTS_FILES SOURCES LIBS ARGS)
+  cmake_parse_arguments(TEST "${opts}" "${oneValArgs}" "${multiValArgs}" ${ARGN})
+  set(TEST_EXECUTABLE "${CMAKE_CURRENT_BINARY_DIR}/${TEST_COMMAND}")
+  add_executable(${TEST_COMMAND} ${TEST_SOURCES})
+  target_link_libraries(${TEST_COMMAND} ${TEST_LIBS})    # Possibly need a boost lib here
+  add_test(NAME ${TEST_NAME} COMMAND ${CMAKE_COMMAND}
+      -DTEST_PROG:FILEPATH=${TEST_EXECUTABLE} 
+      -DTEST_ARGS:STRING=${TEST_ARGS}
+      -DTEST_STDOUT:STRING=${TEST_STDOUT}
+      -DTEST_RESULTS:STRING=${TEST_RESULTS_FILES}
+      -DTEST_RESULTS_DIR:PATH=${TEST_RESULTS_DIR}
+      -P ${SCIMAKE_DIR}/SciTextCompare.cmake
+  )
+  set_tests_properties("${TEST_COMMAND}"
+    PROPERTIES
+    ATTACHED_FILES_ON_FAIL "${RESULTS_FILES}")
+ENDMACRO()
+
+
