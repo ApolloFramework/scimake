7d700425273b5cb4c5e0ef7ffe4f471d4ab449ec
techxdws <techxdws@03405629-b146-45b9-a56f-558d5653c1c1>
2014-01-09 01:35:48 +0000

    Refactoring unit tests under CMake.
    There is now one universal macro, SciAddUnitTest(), which takes the 
    following arguments:
    
    NAME        = the name of this test (which may or may not be the same
                  as the executable)
    COMMAND     = test executable (typically same as NAME, but need not be)
    SOURCES     = 1+ source files to be compiled
    LIBS        = libraries needed to link test
    ARGS        = arguments to test
    EXEC_DIRS   = directories to add to PATH when executing test
    RESULTS     = Files to be compared against golden results. If this
                  var is empty, no comparisons will be done (but see
                  STDOUT below)
    RESULTS_DIR = directory which contains expected results
    STDOUT_FILE = Name of file into which stdout should be captured. This
                  file will be added to $RESULTS so it will be compared
                  against expected output.
     
    Some shenanigans were required because one cannot pass a list to cmake
    via the -D argument. Once we switch to CMake 2.8.11, this will be
    simlified a little, in that we can use the $<SEMICOLON> generator
    instead of what I am currently using, ASCII 001 (ctrl-A).
    
    
    
    git-svn-id: https://ice.txcorp.com/svnrepos/code/scimake/trunk@450 03405629-b146-45b9-a56f-558d5653c1c1

--- SciTextCompare.cmake
+++ SciTextCompare.cmake
@@ -12,20 +12,46 @@
 #
 ######################################################################
 
-# See whether program runs.
+if (WIN32)
+  set(PATH_SEPARATOR ";")
+else ()
+  set(PATH_SEPARATOR ":")
+endif ()
+
 message(STATUS "TEST_ARGS = ${TEST_ARGS}.")
 string(REPLACE "\"" "" ARGS_LIST "${TEST_ARGS}")
 string(REPLACE " " ";" ARGS_LIST "${ARGS_LIST}")
 
-# if TEST_STDOUT is non-empty, then we use it as the output file into
-# for the execute_process(), and we add it to the ${TEST_RESULTS} to
-# be compared. This allows us to have a test which generates one or
+# Because the list of dirs could not actually be passed in a list (due to
+# the use of cmake -D), what we actually got was one long string, with
+# a workaround separator character (ctrl-A, or ASCII 001) in place of the 
+# semicolon. (Once we are able to use CMake 2.8.11 universally, we can use
+# the generator $<SEMICOLON> instead of ctrl-A).
+#
+# Here we replace the workaround separator character with the PATH separator
+# to create the string we need to add to the PATH before running the test.
+
+string(ASCII 1 WORKAROUND_SEPARATOR)
+
+if (TEST_EXEC_DIRS)
+  string(REPLACE "${WORKAROUND_SEPARATOR}" "${PATH_SEPARATOR}"
+         ENV_PATH "${TEST_EXEC_DIRS}")
+  file(TO_NATIVE_PATH "${ENV_PATH}" ENV_PATH)
+  set(ENV{PATH} "${ENV_PATH}${PATH_SEPARATOR}$ENV{PATH}")
+  message(STATUS "ENV{PATH} now set to $ENV{PATH}")
+endif ()
+
+# if TEST_STDOUT_FILE is non-empty, then we use it as the output file
+# into for the execute_process(), and we add it to the ${TEST_RESULTS}
+# to be compared. This allows us to have a test which generates one or
 # more files which are to be compared, while also comparing the stdout
 # of the test.
-if (TEST_STDOUT)
+
+if (TEST_STDOUT_FILE)
   execute_process(COMMAND ${TEST_PROG} ${ARGS_LIST} 
-    RESULT_VARIABLE EXEC_ERROR OUTPUT_FILE ${TEST_STDOUT})
-  set(TEST_RESULTS ${TEST_RESULTS} ${TEST_STDOUT})
+    RESULT_VARIABLE EXEC_ERROR
+    OUTPUT_FILE ${TEST_STDOUT_FILE})
+  set(TEST_RESULTS ${TEST_RESULTS} ${TEST_STDOUT_FILE})
 else ()
   execute_process(COMMAND ${TEST_PROG} ${ARGS_LIST}
     RESULT_VARIABLE EXEC_ERROR)
@@ -36,31 +62,32 @@ if (EXEC_ERROR)
 endif ()
 message(STATUS "Execution succeeded.")
 
-# Test all the output
-set(diffres)
-# There must be an easier way to pass a list
-# message(STATUS "TEST_RESULTS = ${TEST_RESULTS}.")
-string(REPLACE "\"" "" RESULTS_LIST "${TEST_RESULTS}")
-string(REPLACE " " ";" RESULTS_LIST "${RESULTS_LIST}")
-message(STATUS "RESULTS_LIST = ${RESULTS_LIST}.")
-foreach (res ${RESULTS_LIST})
-  if (NOT EXISTS ${res}) 
-    message(FATAL_ERROR "FILE ${res} does not exist.")
-  endif()
-  if (NOT EXISTS ${TEST_RESULTS_DIR}/${res}) 
-    message(FATAL_ERROR "FILE ${TEST_RESULTS_DIR}/${res} does not exist.")
-  endif()
-  execute_process(COMMAND diff --strip-trailing-cr
-    ${res} ${TEST_RESULTS_DIR}/${res}
-    RESULT_VARIABLE DIFFERS)
-  if (DIFFERS)
-    set(diffres ${diffres} "${res}")
-  else ()
-    message(STATUS "Comparison of ${res} succeeded.")
-  endif()
-endforeach ()
-if (diffres)
-  message(FATAL_ERROR "Comparison failure: ${diffres} differ.")
-endif ()
-message(STATUS "Comparison succeeded.")
+if (TEST_RESULTS)
+  # Test all the output
+  # There must be an easier way to pass a list
+  # message(STATUS "TEST_RESULTS = ${TEST_RESULTS}.")
+  string(REPLACE "\"" "" RESULTS_LIST "${TEST_RESULTS}")
+  string(REPLACE " " ";" RESULTS_LIST "${RESULTS_LIST}")
+  message(STATUS "RESULTS_LIST = ${RESULTS_LIST}.")
 
+  foreach (res ${RESULTS_LIST})
+    if (NOT EXISTS ${res}) 
+      message(FATAL_ERROR "FILE ${res} does not exist.")
+    endif()
+    if (NOT EXISTS ${TEST_RESULTS_DIR}/${res}) 
+      message(FATAL_ERROR "FILE ${TEST_RESULTS_DIR}/${res} does not exist.")
+    endif()
+    execute_process(COMMAND diff --strip-trailing-cr
+      ${res} ${TEST_RESULTS_DIR}/${res}
+      RESULT_VARIABLE DIFFERS)
+    if (DIFFERS)
+      set(diffres "${res}")
+    else ()
+      message(STATUS "Comparison of ${res} succeeded.")
+    endif()
+  endforeach ()
+  if (diffres)
+    message(FATAL_ERROR "Comparison failure: ${diffres} differ.")
+  endif ()
+  message(STATUS "Comparison succeeded.")
+endif ()
--- SciUnitTestMacros.cmake
+++ SciUnitTestMacros.cmake
@@ -11,35 +11,54 @@
 #
 ######################################################################
 
-# Add a unit test
-MACRO(add_unit_test)
-  set(oneValArgs NAME COMMAND)
-  set(multiValArgs SOURCES LIBS ARGS)
-  cmake_parse_arguments(TEST "${opts}" "${oneValArgs}" "${multiValArgs}" ${ARGN})
-  add_executable(${TEST_COMMAND} ${TEST_SOURCES})
-  target_link_libraries(${TEST_COMMAND} ${TEST_LIBS})
-  add_test(NAME ${TEST_NAME} COMMAND ${TEST_COMMAND} ${TEST_ARGS})
-ENDMACRO()
+# Add a unit test. If the test needs to compare its results against some
+# expected results, then RESULTS_DIR and RESULTS (or STDOUT) must be set.
+#
+# Args
+#   NAME        = the name of this test (which may or may not be the same
+#                 as the executable)
+#   COMMAND     = test executable (typically same as NAME, but need not be)
+#   SOURCES     = 1+ source files to be compiled
+#   LIBS        = libraries needed to link test
+#   ARGS        = arguments to test
+#   EXEC_DIRS   = directories to add to PATH when executing test
+#   RESULTS     = Files to be compared against golden results. If this
+#                 var is empty, no comparisons will be done (but see
+#                 STDOUT below)
+#   RESULTS_DIR = directory which contains expected results
+#   STDOUT_FILE = Name of file into which stdout should be captured. This
+#                 file will be added to $RESULTS so it will be compared
+#                 against expected output.
 
-# Add a unit test which generates output that needs to be compared against
-# golden output.
-MACRO(add_unit_test_with_comparison)
-  set(oneValArgs NAME COMMAND RESULTS_DIR STDOUT)
-  set(multiValArgs RESULTS_FILES SOURCES LIBS ARGS)
+MACRO(SciAddUnitTest)
+  string(ASCII 1 WORKAROUND_SEPARATOR)
+  set(oneValArgs NAME COMMAND RESULTS_DIR STDOUT_FILE)
+  set(multiValArgs RESULTS_FILES SOURCES LIBS ARGS EXEC_DIRS)
   cmake_parse_arguments(TEST "${opts}" "${oneValArgs}" "${multiValArgs}" ${ARGN})
   set(TEST_EXECUTABLE "${CMAKE_CURRENT_BINARY_DIR}/${TEST_COMMAND}")
   add_executable(${TEST_COMMAND} ${TEST_SOURCES})
   target_link_libraries(${TEST_COMMAND} ${TEST_LIBS})
+
+# Because we can't pass a list to cmake with -D option below, we have to
+# convert the list into a string, meaning we have to get rid of the semi-
+# colons in the string. If we were using CMake 2.8.11, we could convert the
+# semicolons into the generator $<SEMICOLON>, but for now we need to use a
+# workaround separator caharcter which isn't going to be contained in any of
+# the strings that make up the list. We can be pretty certain the ctrl-A
+# (ASCII 001) won't be in any of the strings.
+
+  string(REPLACE ";" "${WORKAROUND_SEPARATOR}" EXEC_DIR_STRING "${TEST_EXEC_DIRS}")
+
   add_test(NAME ${TEST_NAME} COMMAND ${CMAKE_COMMAND}
       -DTEST_PROG:FILEPATH=${TEST_EXECUTABLE} 
       -DTEST_ARGS:STRING=${TEST_ARGS}
-      -DTEST_STDOUT:STRING=${TEST_STDOUT}
+      -DTEST_STDOUT_FILE:STRING=${TEST_STDOUT_FILE}
       -DTEST_RESULTS:STRING=${TEST_RESULTS_FILES}
       -DTEST_RESULTS_DIR:PATH=${TEST_RESULTS_DIR}
+      -DTEST_EXEC_DIRS:STRING=${EXEC_DIR_STRING}
       -P ${SCIMAKE_DIR}/SciTextCompare.cmake
   )
   set_tests_properties("${TEST_COMMAND}"
-    PROPERTIES
-    ATTACHED_FILES_ON_FAIL "${RESULTS_FILES}")
+    PROPERTIES ATTACHED_FILES_ON_FAIL "${RESULTS_FILES}")
 ENDMACRO()
 
