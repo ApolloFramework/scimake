d208da788babb3f7f10075022512443570d441b1
krugers <krugers@03405629-b146-45b9-a56f-558d5653c1c1>
2015-01-19 23:44:21 +0000

    Clean up of FindSciMkl
    Too much hard-coding.  This now allows the order of:
    Command-line specification, environment variable usage (because their
    scripts specify it), and then finally trying the hard-code.
    
    Also, specifying iomp5 by doing a relative search path to Mkl_ROOT_DIR
    
    
    git-svn-id: https://ice.txcorp.com/svnrepos/code/scimake/trunk@724 03405629-b146-45b9-a56f-558d5653c1c1

--- Modules/FindSciMkl.cmake
+++ Modules/FindSciMkl.cmake
@@ -23,59 +23,32 @@
 #
 #
 ######################################################################
+###
+##  Order of precedence: Command-line, environment, hard-code try
+#
+if ("${Mkl_ROOT_DIR}" STREQUAL "")
+  if (NOT "$ENV{MKLROOT}" STREQUAL "")
+     set(Mkl_ROOT_DIR "$ENV{MKLROOT}")
+  endif()
+endif()
 
-if (WIN32)
-  #foreach(year 2011 2012 2013)
-    #set(Mkl_ROOT_DIR "C:/Program Files (x86)/Intel/Composer XE ${year}/mkl/lib/intel64")
+# Try hard-code directory if not specified already
+if ("${Mkl_ROOT_DIR}" STREQUAL "")
+  if (WIN32)
     set(Mkl_ROOT_DIR "C:/Program Files (x86)/Intel/Composer XE/mkl")
-    set(Mkl_INCLUDE_DIRS "${Mkl_ROOT_DIR}/include")
-      SciFindPackage(PACKAGE "Mkl"
-                    LIBRARIES "mkl_intel_lp64;mkl_intel_thread;mkl_core"
-                    INCLUDE_SUBDIRS "include"
-                    LIBRARY_SUBDIRS "lib/intel64"
-                    )
-    #set(Iomp5_ROOT_DIR "C:/Program Files (x86)/Intel/Composer XE ${year}/compiler/lib/intel64")
-    set(Iomp5_ROOT_DIR "C:/Program Files (x86)/Intel/Composer XE/compiler/lib/intel64")
-      SciFindPackage(PACKAGE "Iomp5"
-                    LIBRARIES "libiomp5md"
-                    )
-    if (MKL_FOUND)
-      message(STATUS "Mkl found.")
-      set(HAVE_MKL 1 CACHE BOOL "Whether have Mkl")
-      break()
-    endif ()
-  #endforeach()
-else (WIN32)
-    if (NOT Mkl_ROOT_DIR)
-      set(Mkl_ROOT_DIR "/usr/local/intel/mkl")
-    endif ()
-    if (NOT Iomp5_ROOT_DIR)
-      set (Iomp5_ROOT_DIR "${Mkl_ROOT_DIR}/../lib/intel64")
-    endif ()
-    set(Mkl_INCLUDE_DIRS "${Mkl_ROOT_DIR}/include")
-      if (NOT LINK_WITH_MKL_SHARED)
-        SciFindPackage(PACKAGE "Mkl"
-                       LIBRARIES "libmkl_intel_lp64.a;libmkl_intel_thread.a;libmkl_core.a"
-                       INCLUDE_SUBDIRS "include"
-                       LIBRARY_SUBDIRS "lib/intel64"
-                       )
-      else ()
-         # On iter one needs to link against the shared MKL libraries
-         SciFindPackage(PACKAGE "Mkl"
-                       LIBRARIES "mkl_intel_lp64;mkl_intel_thread;mkl_core"
-                       INCLUDE_SUBDIRS "include"
-                       LIBRARY_SUBDIRS "lib/intel64"
-                       )
+  else (WIN32)
+    set(Mkl_ROOT_DIR "/usr/local/intel/mkl")
+  endif (WIN32)
+endif()
 
-      endif ()
-    SciFindPackage(PACKAGE "Iomp5"
-                   LIBRARIES "iomp5"
-                   )
-    if (MKL_FOUND)
-      message(STATUS "Mkl found.")
-      set(HAVE_MKL 1 CACHE BOOL "Whether have Mkl")
-    endif ()
-endif (WIN32)
+###
+##  Now start the searching
+#
+SciFindPackage(PACKAGE "Mkl"
+              LIBRARIES "mkl_intel_lp64;mkl_intel_thread;mkl_core"
+              INCLUDE_SUBDIRS "include"
+              LIBRARY_SUBDIRS "lib/intel64"
+              )
 
 if (NOT MKL_FOUND)
   message(STATUS "Did not find Mkl.  Use -DMkl_ROOT_DIR and, if using OpenMP, Iomp5_ROOT_DIR to specify the installation directory.")
@@ -83,4 +56,16 @@ if (NOT MKL_FOUND)
     message(FATAL_ERROR "Finding MKL failed.")
   endif ()
 endif ()
+if (MKL_FOUND)
+  message(STATUS "Mkl found.")
+  set(HAVE_MKL 1 CACHE BOOL "Whether have Mkl")
+endif ()
 
+###
+##  IOMP5 is sometimes needed to get link to work.  
+##  Go ahead and find it to be available.
+#
+#  Set iomp_dir
+get_filename_component(Iomp5_ROOT_DIR ${Mkl_ROOT_DIR}/../compiler/lib/intel64 REALPATH)
+
+SciFindPackage(PACKAGE "Iomp5" LIBRARIES "iomp5")
