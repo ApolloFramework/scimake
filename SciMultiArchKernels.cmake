f8bbbc3052c4b63946198ab7341497e34a04d5c4
dmeiser <dmeiser@03405629-b146-45b9-a56f-558d5653c1c1>
2015-11-06 18:47:43 +0000

    First cut at support for multi-arch kernels.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/scimake/branches/reduceISAsBuilt-r906-dmeiser@913 03405629-b146-45b9-a56f-558d5653c1c1

---
+++ SciMultiArchKernels.cmake
@@ -0,0 +1,31 @@
+######################################################################
+#
+# SciMultiArchKernels: Capabilities for building multiarch libraries
+#
+# $Id$
+#
+# Copyright 2010-2015, Tech-X Corporation, Boulder, CO.
+# See LICENSE file (EclipseLicense.txt) for conditions of use.
+#
+#
+######################################################################
+
+message("")
+message(STATUS "--------- Setting up multi-arch capabilities ---------")
+
+######################################################################
+# Define the set of architectures we're building for
+######################################################################
+set(ALL_INSTRUCTION_SETS Generic SSE2 SSE3 AVX AVX2 AVX512)
+option(SCI_BUILD_ALL_INSTRUCTION_SETS
+       "Whether to build for all architectures"
+       FALSE)
+if (SCI_BUILD_ALL_INSTRUCTION_SETS)
+  SciPrintString("Building multiarch kernels for all of ISAs")
+  set(SCI_MULTIARCH_INSTRUCTION_SETS ${ALL_INSTRUCTION_SETS})
+else ()
+  SciPrintString("Building multiarch kernels for minimal set of ISAs")
+  set(SCI_MULTIARCH_INSTRUCTION_SETS Generic ${SCI_MOST_POWERFUL_ISA})
+endif ()
+SciPrintString("SCI_MULTIARCH_INSTRUCTION_SETS == ${SCI_MULTIARCH_INSTRUCTION_SETS}")
+message(STATUS "------------------------------------------------------")
