91a630cb1a8d3facbc978adb704d281b24d774a7
cary <cary@03405629-b146-45b9-a56f-558d5653c1c1>
2015-11-27 14:52:33 +0000

    HDF5 again moved its configuration file.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/scimake/trunk@943 03405629-b146-45b9-a56f-558d5653c1c1

--- Modules/FindSciHdf5.cmake
+++ Modules/FindSciHdf5.cmake
@@ -70,9 +70,11 @@ message(STATUS "Hdf5_ROOT_DIR = ${Hdf5_ROOT_DIR}.")
 # Version known, can look for config file
 SciFindPackage(PACKAGE "Hdf5"
   CONFIG_SUBDIRS
+# They keep changing the location
     lib/cmake/hdf5-${Hdf5_VERSION}   # 1.8.6
     share/cmake/hdf5-${Hdf5_VERSION} # 1.8.7
-    share/cmake/hdf5                # 1.8.12
+    share/cmake/hdf5   # 1.8.12
+    share/cmake        # 1.8.18
   # USE_CONFIG_FILE # Cannot always source, so decide later
   CONFIG_FILE_ONLY
   FIND_QUIETLY
