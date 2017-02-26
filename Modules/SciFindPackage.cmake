afaf84f3f2c96e45143c9a30ee14709277de447c
cary <cary@03405629-b146-45b9-a56f-558d5653c1c1>
2017-02-26 17:59:41 +0000

    Do not search in system paths if a root path given.  This prevents finding, e.g., a random dll (in this case hdf5.dll in a visit installation path) aa that is not correct.
    This may have unforeseen consequences.  have to wait , see,
    and fix as needed.
    
    
    
    
    git-svn-id: https://ice.txcorp.com/svnrepos/code/scimake/trunk@1138 03405629-b146-45b9-a56f-558d5653c1c1

--- Modules/SciFindPackage.cmake
+++ Modules/SciFindPackage.cmake
@@ -501,9 +501,10 @@ function(SciFindPkgFiles pkgname pkgfiles
     endif ()
 
 # If not found, try again with default paths
-    if (NOT ${pkgfilevar})
+    if ((NOT ${pkgfilevar}) AND (NOT rootpath))
       if (DEBUG_CMAKE)
         message(STATUS "Failed to find ${realpkgfile} in search path, trying default paths.")
+        message(STATUS "rootpath = ${rootpath}.")
       endif ()
       if (${singularsfx} STREQUAL PROGRAM)
         find_program(${basesrchargs}
